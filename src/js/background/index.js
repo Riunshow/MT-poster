18f398d2d593f96639d1cfd8b98e71e7bfd79ce9
Riunshow <zhubotaigg@gmail.com>
2018-09-11 14:46:35 +0800

    人物,边框,遮罩层移动

--- src/js/background/index.js
+++ src/js/background/index.js
@@ -3,87 +3,118 @@ export function background(res) {
   const bg1 = PIXI.Sprite.fromImage(res['./bg1.jpg'].url)
 
   // 初始化
-  const app = new PIXI.Application(window.innerWidth, window.innerHeight / 1.5)
+  const app = new PIXI.Application(window.innerWidth, window.innerHeight / 1.5, {backgroundColor: 0xFFFFFF})
 
   // 缩放比
   const scaleWidth = window.innerWidth / bg1.width
-  const scaleheight = window.innerHeight / bg1.height
+  const scaleheight = window.innerHeight / 1.5 / bg1.height
 
-  document.body.appendChild(app.view);
+  document.body.appendChild(app.view)
 
   bg1.scale.set(scaleWidth, scaleheight)
 
   app.stage.interactive = true
 
   // 添加背景图片
-  app.stage.addChild(bg1);
+  // app.stage.addChild(bg1)
 
+  // 创建一个容器
+  const container = new PIXI.Container()
 
+  // 引入图片
+  const texture = PIXI.Texture.fromImage(require('./../../../assets/male.png'))
+  texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST
 
-
-  // create a texture from an image path
-  var texture = PIXI.Texture.fromImage('https://pixijs.io/examples/required/assets/bunny.png');
-
-  // Scale mode for pixelation
-  texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
-
-  createBunny(app.screen.width * 0.5, app.screen.height * 0.5);
+  // 创建人物图片
+  createBunny(app.screen.width * 0.5, app.screen.height * 0.5)
 
   function createBunny(x, y) {
+    // 创建人物图片
+    const bunny = new PIXI.Sprite(texture)
+    bunny.interactive = true
+    bunny.buttonMode = true
+    bunny.anchor.set(0.5)
+    // bunny.scale.set(1)
 
-    // create our little bunny friend..
-    var bunny = new PIXI.Sprite(texture);
+    bunny.height = 300
+    bunny.width = 150
 
-    // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
-    bunny.interactive = true;
+    container.addChild(bunny)
 
-    // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
-    bunny.buttonMode = true;
+    container.x = x
+    container.y = y
 
-    // center the bunny's anchor point
-    bunny.anchor.set(0.5);
+    console.log(bunny.x, bunny.y, bunny.width, bunny.height)
+    console.log(container.x, container.y, container.width, container.height)
 
-    // make it a bit bigger, so it's easier to grab
-    bunny.scale.set(3);
+    // border padding
+    const PADDING = 100
 
-    // setup events for mouse + touch using
-    // the pointer events
-    bunny
+    // border height width
+    const borderWidth = container.width + PADDING * 2
+    const borderHeight = container.height + PADDING * 2
+
+    // 创建外边框
+    const border = new PIXI.Graphics()
+    const borderX = 0 - borderWidth / 2
+    const borderY = 0 - borderHeight / 2
+
+    // 边框设置
+    border.lineStyle(2, 0x000000, 1)
+    border.drawRect(borderX, borderY, borderWidth, borderHeight)
+
+    container.addChild(border)
+    // container.border = border
+
+    // 创建遮罩层
+    const handler = new PIXI.Graphics()
+    handler.beginFill(0xffFF00, 0.1)
+    handler.drawRect(borderX, borderY, borderWidth, borderHeight);
+    handler.endFill();
+
+    handler.interactive = true;
+
+    container.addChild(handler)
+    container.handler = handler;
+
+    handler
       .on('pointerdown', onDragStart)
       .on('pointerup', onDragEnd)
       .on('pointerupoutside', onDragEnd)
-      .on('pointermove', onDragMove);
+      // .on('pointermove', onDragMove)
+      .on('mousemove', onDragMove)
+      .on('touchmove', onDragMove);
+    // add to app
+    app.stage.addChild(container)
 
-    // move the sprite to its designated position
-    bunny.x = x;
-    bunny.y = y;
+    // app.stage.addChild(border)
+    // app.stage.border = border
 
-    // add it to the stage
-    app.stage.addChild(bunny);
-  }
-
-  function onDragStart(event) {
-    // store a reference to the data
-    // the reason for this is because of multitouch
-    // we want to track the movement of this particular touch
-    this.data = event.data;
-    this.alpha = 0.5;
-    this.dragging = true;
-  }
-
-  function onDragEnd() {
-    this.alpha = 1;
-    this.dragging = false;
-    // set the interaction data to null
-    this.data = null;
-  }
-
-  function onDragMove() {
-    if (this.dragging) {
-      var newPosition = this.data.getLocalPosition(this.parent);
-      this.x = newPosition.x;
-      this.y = newPosition.y;
+    function onDragStart(event) {
+      this.data = event.data
+      this.alpha = 0.5
+      this.dragging = true
     }
-  }
 
+    function onDragEnd() {
+      this.alpha = 1
+      this.dragging = false
+      this.data = null
+    }
+
+    function onDragMove() {
+      if (this.dragging) {
+        var newPosition = this.data.getLocalPosition(this.parent)
+        this.x = newPosition.x
+        this.y = newPosition.y
+
+        border.x = this.x
+        border.y = this.y
+
+        bunny.x = this.x
+        bunny.y = this.y
+      }
+    }
+
+  }
 }
